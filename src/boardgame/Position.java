a62ec59f8fae3dcde9e756043cb3ae69b09e1bda
victors11 <joaovdesousa2001@gmail.com>
2021-07-20 15:20:07 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,39 @@
+package boardgame;
+
+/**
+ * this is an auxiliary class, responsible for representing the position of a piece on the board.
+ * 
+ * @author João Victor
+ */
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
---
+++ src/boardgame/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package corresponds to the game board layer. It contains the elements that are part of a chess game, 
+ * that is, the pieces and the board. 
+ * 
+ * @author João Victor
+ */
+package boardgame;
