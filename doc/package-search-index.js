9465c55a6ea52d87a167f5a28e6d19771172c726
victors11 <joaovdesousa2001@gmail.com>
2021-08-02 23:44:00 -0300

    Javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"boardgame"},{"l":"chess"},{"l":"chess.pieces"}];updateSearchResults();
--- src/application/UI.java
+++ src/application/UI.java
@@ -144,7 +144,7 @@ public class UI {
 	 */
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
-			System.out.print(ANSI_PURPLE_BACKGROUND);
+			System.out.print(ANSI_RED_BACKGROUND);
 		}
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
